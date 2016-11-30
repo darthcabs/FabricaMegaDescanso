7f9d52039c3c9f1109b2593d1ea01153ba86fe87
Kenzo Yuri <kenzonishitani@gmail.com>
2016-11-30 20:13:11 -0200

    Criada a tebela Pergunta

---
+++ Fiap.Projeto.Database/Pergunta.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Pergunta]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Titulo] VARCHAR(25) NOT NULL, 
+    [Descricao] TEXT NOT NULL, 
+    [Tag] VARCHAR(25) NOT NULL, 
+    [Data] DATETIME NOT NULL, 
+    [RespostaEscolhida] INT NULL
+)
