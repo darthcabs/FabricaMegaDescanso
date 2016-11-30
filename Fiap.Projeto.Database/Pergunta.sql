89e1652b96bc0086a49f2fd3a07ece497a37dd89
Kenzo Yuri <kenzonishitani@gmail.com>
2016-11-30 20:35:02 -0200

    Feito o relacionamento de Aluno e Pergunta

--- Fiap.Projeto.Database/Pergunta.sql
+++ Fiap.Projeto.Database/Pergunta.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[Pergunta]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL , 
+	[AlunoRm] INT NOT NULL, 
     [Titulo] VARCHAR(25) NOT NULL, 
     [Descricao] TEXT NOT NULL, 
     [Tag] VARCHAR(25) NOT NULL, 
     [Data] DATETIME NOT NULL, 
-    [RespostaEscolhida] INT NULL
+    [RespostaEscolhida] INT NULL,   
+    CONSTRAINT [FK_Pergunta_Aluno] FOREIGN KEY ([AlunoRm]) REFERENCES [Aluno]([Rm]), 
+    PRIMARY KEY ([Id], [AlunoRm])
 )
