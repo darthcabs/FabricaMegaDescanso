e43160695b443b539dd0b061109bd103a293a798
Kenzo Yuri <kenzonishitani@gmail.com>
2016-11-30 21:28:31 -0200

    Corrigido erro na criação das tabelas

--- Fiap.Projeto.Database/Resposta.sql
+++ Fiap.Projeto.Database/Resposta.sql
@@ -2,10 +2,11 @@
 (
 	[Id] INT NOT NULL , 
 	[PerguntaId] INT NOT NULL,
+	[Autor] INT NOT NULL,
 	[AlunoRm] INT NOT NULL,
     [Descricao] TEXT NOT NULL, 
     [Data] DATETIME NOT NULL,     
     PRIMARY KEY ([Id], [PerguntaId]), 
-    CONSTRAINT [FK_Resposta_Pergunta] FOREIGN KEY ([PerguntaId]) REFERENCES [Pergunta]([Id]), 
-    CONSTRAINT [FK_Resposta_Aluno] FOREIGN KEY ([AlunoRm]) REFERENCES [Aluno]([Rm])
+    CONSTRAINT [FK_Resposta_Aluno] FOREIGN KEY ([AlunoRm]) REFERENCES [Aluno]([Rm]), 
+	CONSTRAINT [FK_Resposta_Pergunta] FOREIGN KEY ([PerguntaId],[Autor]) REFERENCES [Pergunta]([Id],[AlunoRm])
 )
