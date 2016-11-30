49a040eeab761bd9adb905cbe31438150142f414
Kenzo Yuri <kenzonishitani@gmail.com>
2016-11-30 20:51:33 -0200

    Deletado o esboço dos models para gerar através do banco

--- OverflowedStack/Models/Autor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fiap.Projeto.Web
-{
-    public class Autor
-    {
-        public string Nome { get; set; }
-        public int RM { get; set; }
-        public string Senha { get; set; }
-    }
-}
