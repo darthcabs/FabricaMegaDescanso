b06af5ba1a0c20388287e6f73876e51fbb9684e8
Kenzo Yuri <kenzonishitani@gmail.com>
2016-12-01 20:21:41 -0200

    Cadastrar aluno (Controller)

--- OverflowedStack/Controllers/AlunoController.cs
+++ OverflowedStack/Controllers/AlunoController.cs
@@ -1,5 +1,6 @@
 ﻿using OverflowedStack.Models;
 using OverflowedStack.UnitsOfWork;
+using OverflowedStack.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,39 @@ namespace OverflowedStack.Controllers
     public class AlunoController : Controller
     {
 
+        private UnitOfWork _unit = new UnitOfWork();
+
+        #region Get
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Post
+        [HttpPost]
+        public ActionResult Cadastrar(AlunoViewModel alunoViewModel)
+        {
+            var aluno = new Aluno()
+            {
+                Rm = alunoViewModel.Rm,
+                Nome = alunoViewModel.Nome,
+                Senha = alunoViewModel.Senha
+            };
+            _unit.AlunoRepository.Cadastrar(aluno);
+            _unit.Salvar();
+            return RedirectToAction("Cadastrar");
+        }
+        #endregion
+
+        #region Dispose
+        protected override void Dispose(bool disposing)
+        {
+            _unit.Dispose();
+            base.Dispose(disposing);
+        }
+        #endregion
 
     }
 }
