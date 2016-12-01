f556998e3d0ec5d277a7582f9574d87d9a178a07
Kenzo Yuri <kenzonishitani@gmail.com>
2016-12-01 19:01:34 -0200

    Criado o UnitOfWork

---
+++ OverflowedStack/UnitsOfWork/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Fiap.Projeto.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OverflowedStack.UnitsOfWork
+{
+    public class UnitOfWork : IDisposable
+    {
+        #region Fields
+        private Entidades _context = new Entidades();
+        #endregion
+
+        //Properties...
+
+        #region Save Changes
+        public void Salvar()
+        {
+            _context.SaveChanges();
+        }
+        #endregion
+
+        #region Dipose
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
